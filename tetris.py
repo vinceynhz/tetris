b9fd8138fd8bf3f8870cbd05f1e361f97ff2cc90
vinceynhz <vince.ynhz@gmail.com>
2019-09-01 14:31:07 -0500

    Adding next shape box and level/score lines

--- tetris.py
+++ tetris.py
@@ -21,57 +21,112 @@ import time
 print(u"\u2588\u2588")
 """
 
-class FG:
-    BLACK = u"\u001b[30m"
-    RED = u"\u001b[31m"
-    GREEN = u"\u001b[32m"
-    YELLOW = u"\u001b[33m"
-    BLUE = u"\u001b[34m"
-    MAGENTA = u"\u001b[35m"
-    CYAN = u"\u001b[36m"
-    WHITE = u"\u001b[37m"
+class COLOR:
     RESET = u"\u001b[0m"
+    class FG:
+        BLACK = u"\u001b[30m"
+        RED = u"\u001b[31m"
+        GREEN = u"\u001b[32m"
+        YELLOW = u"\u001b[33m"
+        BLUE = u"\u001b[34m"
+        MAGENTA = u"\u001b[35m"
+        CYAN = u"\u001b[36m"
+        WHITE = u"\u001b[37m"
+    class BG:
+        LIGHT = ""
 
-class BG:
-    LIGHT = ""    
+
+class BOX:
+    TL_CORNER = u"\u250c"
+    TR_CORNER = u"\u2510"
+    BL_CORNER = u"\u2514"
+    BR_CORNER = u"\u2518"
+    H_LINE = u"\u2500"
+    V_LINE = u"\u2502"
+
+    def top(size):
+        raw(BOX.TL_CORNER)
+        for i in range(size): 
+            raw(BOX.H_LINE)
+        raw(BOX.TR_CORNER)
+
+    def bottom(size):
+        raw(BOX.BL_CORNER)
+        for i in range(size):
+            raw(BOX.H_LINE)
+        raw(BOX.BR_CORNER)
+
+
+BLOCK = u"\u2588\u2588"
+
 
 class Field:
     def __init__(self):
         self.dim = [ [ '.' for x in range(10) ] for y in range(20) ]
 
     def print(self):
-        raw(u"\n \u250c")
-        for i in range(20): # 10 cells * 2 characters for each one
-            raw(u"\u2500")
-        raw(u"\u2510\n")
+        clear()
+
+        # First line of the box
+        raw("\n ")
+        BOX.top(20) # 10 cells * 2 characters for each one
+        raw("\n")
+        
         for i in range(20):
-            raw(u" \u2502")
+            raw(" ", BOX.V_LINE)
             for j in range(10):
                 cell = self.dim[i][j]
                 flag = (j + (i % 2)) % 2 == 0
                 if flag: 
                     raw(u"\u001b[38;5;153m")
-                raw(u"\u2588\u2588")
+                raw(BLOCK)
                 if flag:
-                   raw(u"\u001b[0m")
-            raw(u"\u2502\n")
-        raw(u" \u2514")
-        for i in range(20):
-            raw(u"\u2500")
-        raw(u"\u2518\n")
+                   raw(COLOR.RESET)
+            raw(BOX.V_LINE, "\n")
+        
+        # Last line of the box
+        raw(" ")
+        BOX.bottom(20)
+        raw("\n")
 
-def clear():
+        # Draw next shape box
+        move(30, 2)
+        BOX.top(8)
+
+        move(30, 5)
+        BOX.bottom(8)
+
+        move(30, 10)
+        raw("Level:")
+
+        move(30, 11)
+        raw("Score:")
+
+
+def move(x, y):
+    """
+    ANSI sequence to move the cursor: 
+    CSI y;xf    - move cursor to position y;x 
+                - where y represents the line from the top 
+                - and x the column from the left
+    """
+    raw(u"\u001b["+ str(y) + ";" + str(x) + "f")
+
+
+def clear(x = 1, y = 1):
     """ 
     Call ANSI sequences
     CSI 2J      - erase all screen
-    CSI 1;1f    - move cursor to top left
     """
-    raw(u"\u001b[2J\u001b[1;1f")
+    raw(u"\u001b[2J")
+    move(x, y)
+
 
 def raw(*args):
     for arg in args:
         sys.stdout.write(arg)
     sys.stdout.flush()
+
 
 def main():
     if 'win' in sys.platform:
@@ -81,16 +136,11 @@ def main():
         kernel32 = ctypes.windll.kernel32
         kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
-    # We start by clearing the screen
-    clear()
-    # raw(u"\u001b[48;5;239m\u001b[K");
-
     f = Field()
     f.print()
 
     # raw(u"\u2588\u2588\u2588\u2588\n\u2588\u2588\u2588\u2588\n\u001b[48;5;239m\u001b[K\n")
     
-    # raw(u"\u001b[0m")
     time.sleep(2)
     
     # We clear the screen before we finish
